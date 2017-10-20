cd6b3829c58d8afa5ddf579aff8187d3277ed833
tommylinks <tommylinks1@gmail.com>
2017-10-20 12:47:38 +0300

    dist init

--- gulpfile.js
+++ gulpfile.js
@@ -27,7 +27,6 @@ gulp.task('common-js', function() {
 
 gulp.task('js', ['common-js'], function() {
 	return gulp.src([
-		'app/libs/jquery/dist/jquery.min.js',
 		'app/js/common.min.js', // Всегда в конце
 		])
 	.pipe(concat('scripts.min.js'))
